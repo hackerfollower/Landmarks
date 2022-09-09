932571f57797f9a33852a23b12c3752bb5a661e9
hackerfollower <hackerfollower@gmail.com>
2022-09-09 16:36:20 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Liu Hao on 2022/9/9.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
